3e6515e55b39b8d4c1048aafc411b9419e3d52dc
wennitao <wennital@126.com>
2022-12-21 15:40:47 +0800

    init push

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-cd /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/RTANN/src
-nvcc -o juno_rt.optixir --optix-ir -I /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/include/ -I /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/RTANN/include/ -I /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/SDK/ -I /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/SDK/support -I /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/SDK/build juno_rt.cu
-cd /home/zhliu/workspace/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/RTANN/build
+cd /home/wtni/RTANN/RTANN/src
+nvcc -o juno_rt.optixir --optix-ir -I /home/wtni/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/include/ -I /home/wtni/RTANN/RTANN/include/ -I /home/wtni/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/SDK/ -I /home/wtni/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/SDK/support -I /home/wtni/NVIDIA-OptiX-SDK-7.5.0-linux64-x86_64/SDK/build juno_rt.cu
+cd /home/wtni/RTANN/RTANN/build
 cmake ..
 make
